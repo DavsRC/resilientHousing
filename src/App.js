8feea051a23412dfeef3e7ceed681ca9962e578f
Davinson Rincon <davinson.rincon@est.iudigital.edu.co>
2022-10-10 17:10:01 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from './components/navbar'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <div className="main">
+      <Sidebar />
+      <div className="container">
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default App
